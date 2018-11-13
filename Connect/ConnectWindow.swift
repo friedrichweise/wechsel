f887954ca7e3725de0458ca13cd53c9524d64db5
friedrichweise <account@weise.io>
2018-11-13 15:24:01 +0100

    add first tableview and make whole window borderless and keyWindow

---
+++ Connect/ConnectWindow.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectWindow.swift
+//  Connect
+//
+//  Created by Friedrich Weise on 13.11.18.
+//  Copyright © 2018 Friedrich Weise. All rights reserved.
+//
+
+import Cocoa
+
+class ConnectWindow: NSWindow {
+    override var canBecomeKey: Bool {
+        get {
+            return true
+        }
+    }
+}
--- Connect/ViewController.swift
+++ Connect/ViewController.swift
@@ -9,14 +9,32 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    @IBOutlet var tableView: NSTableView!
+    var data: [[String: String]]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let bridge = blueutilBridge()
-
+        data = [
+            [
+                "name" : "Kusifiri",
+                "status" : "5532612"
+            ],
+            [
+                "name" : "UE Boom 2",
+                "status" : "54329987"
+            ],
+            [
+                "name" : "JBL Go",
+                "status" : "53122543"
+            ]
+        ]
+        tableView.reloadData()
+        let indexSet = IndexSet(integer: 0)
+        tableView.selectRowIndexes(indexSet, byExtendingSelection: false)
         // Do any additional setup after loading the view.
     }
-
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
@@ -26,3 +44,55 @@ class ViewController: NSViewController {
 
 }
 
+extension ViewController: NSTableViewDataSource {
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return data?.count ?? 0
+    }
+    
+}
+
+extension ViewController: NSTableViewDelegate {
+    
+    fileprivate enum CellIdentifiers {
+        static let NameCell = "name"
+        static let StatusCell = "status"
+    }
+    
+    
+   /* func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
+        return 40.0
+    }*/
+    
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        return true;
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        
+        var text: String = ""
+        var cellIdentifier: String = ""
+        
+        // 1
+        guard let item = data?[row] else {
+            return nil
+        }
+        
+        // 2
+        if tableColumn == tableView.tableColumns[0] {
+            text = item["name"]!
+            cellIdentifier = CellIdentifiers.NameCell
+        } else if tableColumn == tableView.tableColumns[1] {
+            text = item["status"]!
+            cellIdentifier = CellIdentifiers.StatusCell
+        }
+        
+        // 3
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = text
+            return cell
+        }
+        return nil
+    }
+    
+}
