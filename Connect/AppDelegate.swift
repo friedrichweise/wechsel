fa851de739e19652f88122da4ad68031ce0130a8
friedrichweise <account@weise.io>
2018-11-13 12:35:06 +0100

    Initial Commit

---
+++ Connect/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Connect
+//
+//  Created by Friedrich Weise on 13.11.18.
+//  Copyright © 2018 Friedrich Weise. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Connect/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Connect
+//
+//  Created by Friedrich Weise on 13.11.18.
+//  Copyright © 2018 Friedrich Weise. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
