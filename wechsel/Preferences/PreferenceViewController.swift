55a6ef9f5b6e9c3dfad9d93919cbaa29299b1fc1
friedrichweise <account@weise.io>
2019-01-26 17:43:23 +0100

    basic launch at login functionality, closes #8

--- wechsel/Preferences/PreferenceViewController.swift
+++ wechsel/Preferences/PreferenceViewController.swift
@@ -8,18 +8,21 @@
 
 import Cocoa
 import MASShortcut
+import LaunchAtLogin
 
 class PreferenceViewController: NSViewController {
     @IBOutlet weak var shortcutView: MASShortcutView!
+    @IBOutlet weak var openAtLoginCheckbox: NSButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         shortcutView.associatedUserDefaultsKey = Config.key
+        openAtLoginCheckbox.state = LaunchAtLogin.isEnabled ? .on : .off
     }
     @IBAction func saveButtonClicked(_ sender: Any) {
+        LaunchAtLogin.isEnabled = openAtLoginCheckbox.state == .on ? true : false
         if let window = self.view.window {
             window.close()
         }
     }
-
 }
